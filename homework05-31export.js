5e017f50a7d1430d4d50966e20dec0ac4de0bb02
Sacha-gh <sacha-u@hotmail.com>
2024-06-03 09:05:48 +0100

    Add files via upload

---
+++ homework05-31export.js
@@ -0,0 +1,3 @@
+import { add } from "../JavaScript/homework05-31.js";
+
+console.log(add(20, 30));
--- homework05-31import.js
+++ homework05-31import.js
@@ -1,5 +1,1 @@
 export let times = (x = 1, y = 1) => x * y;
-
-import { add } from "../JavaScript/homework05-31.js";
-
-console.log(add(20, 30));
